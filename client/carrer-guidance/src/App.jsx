df1c3679ab89b64d272a1c2b8af866a6a62d2614
Jayanth <jayanthjay295@gmail.com>
2024-09-18 19:31:06 +0530

    updated road map

--- client/carrer-guidance/src/App.jsx
+++ client/carrer-guidance/src/App.jsx
@@ -5,6 +5,7 @@ import CareerGuidePage from './pages/Components/CareerForm';
 import Navbar from './pages/Components/LangingPage/Navbar';
 import CareerGuidanceRoadmap from './pages/Components/CareerGuidanceRoadmap';
 import ChatIcon from './pages/Components/ChatIcon';
+import Roadmap from './pages/Components/CareerGuidanceRoadmap';
 import Footer from './pages/Components/LangingPage/Footer';
 import AssessmentPage from './pages/Components/AptitudeStrendthTest/AssessmentFlow'; 
 import Quiz from './pages/Components/AptitudeStrendthTest/Quiz/Main'; 
@@ -20,6 +21,7 @@ const App = () => {
           <Route path="/career-guidance-roadmap" element={<CareerGuidanceRoadmap />} />
           <Route path="/assessment" element={<AssessmentPage />} /> 
           <Route path="/quiz" element={<Quiz />} /> 
+          <Route path="/roadmap" element={<Roadmap />} /> 
         </Routes>
         <ChatIcon />
         <Footer />
